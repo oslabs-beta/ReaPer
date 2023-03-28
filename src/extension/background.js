bb0812d51ef2203ddab48765673873d47310430a
annako-io <annako1@icloud.com>
2023-03-27 20:04:20 -0700

    boiler plate continued.
    Co-authored-by: Nancy Yu nancyyu09@gmail.com
    Co-authored-by: Katrina McDonagh katrinamcdonagh385@gmail.com
    Co-authored-by: Jamie Lee jaamiieelee@gmail.com
    Co-authored-by: Anna Ko 45987428+annako-io@users.noreply.github.com
    Co-authored-by: Michael Arita michael.arita01@gmail.com

--- src/extension/background.js
+++ src/extension/background.js
@@ -1,8 +1,8 @@
 /**
  * Invoked when a message from the dev tool has been received.
- * @param msg (object) - 
+ * @param msg (object) - Object with a "message" property containing the message
  */
 const onMessageFromDevTool = msg => {
   console.log('background.js received a message from the dev tool:', msg.message);
-  sendMessageToContentScript(msg.message);
+  
 }
