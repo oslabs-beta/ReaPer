091f3ac13d06717bf778ac139673373d2d344402
Jamie <jaamiieelee@gmail.com>
2023-03-30 15:05:22 -0700

    Added css for ComponentTree.jsx and App.jsx, added temporary divs for demo purposes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import MainNav from './components/MainNav';
 import ComponentTree from './components/ComponentTree.jsx';
+import RenderEvents from './components/RenderEvents';
 
 function App() {
   return (
-    <div id="container">
+    <div id='container'>
       <MainNav />
+      <div className='render-events'>Rendered Events and Duration</div>
+      <div className='render-comp'>Rendered Components</div>
+      <div className='comp-ranked'>Components Ranked by Render Time</div>
       <ComponentTree />
     </div>
+    // <RenderEvents />
   );
 }
 
---
+++ src/app/components/RenderEvents.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
+
+const data = {
+  datasets: [
+    {
+      label: 'My First Dataset',
+      data: [65, 59, 80, 81, 56, 55, 40],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)',
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const RenderEvents = () => {
+  return (
+    <div>
+      <Bar data={data} />
+    </div>
+  );
+};
+
+export default RenderEvents;
