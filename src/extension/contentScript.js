73e85264acf8bec9e86f073997bdb40b81bcc7dd
Nancy Yu <nancyyu09@gmail.com>
2023-04-01 19:54:25 -0700

    Attempted to connect to rdtFiber

--- src/extension/contentScript.js
+++ src/extension/contentScript.js
@@ -14,12 +14,32 @@ const sendMessageToBackground = (msg) => {
  */
 const handleMessageFromBackground = msg => {
   console.log('contentScript.js: received message from background:', msg);
+  // console.log('window:', Window);
+  // console.log(Window.__REACT_DEVTOOLS_GLOBAL_HOOK);
+  // console.log(Window.document.title);
   return false;
 };
 
-// sendMessageToBackground('HELLO FROM CONTENT.JS');
+sendMessageToBackground('HELLO FROM CONTENT.JS');
 
 /**
 Set up listener for messages from the background script
 */
 chrome.runtime.onMessage.addListener(handleMessageFromBackground);
+
+
+// const url = chrome.runtime.getURL('./bundles/backend.bundle.js');
+
+// const scriptElement = document.createElement('script');
+// scriptElement.onload = function () {
+//   //window.myFunction(); // This will invoke the myFunction function
+//   console.log('hellllllllllllllllllllllllllllllllllllllllllllllllllo');
+// };
+// scriptElement.src = url;
+// document.head.appendChild(scriptElement);
+
+// (async () => {
+//   const src = chrome.runtime.getURL('bundles/backend.bundle.js');
+//   const contentMain = await import(src);
+//   await contentMain.default();
+// })();
