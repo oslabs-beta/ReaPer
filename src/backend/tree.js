d8f17540b9025ab596f656e2e43b6b1214053a13
Nancy Yu <nancyyu09@gmail.com>
2023-04-03 10:39:32 -0700

    removed commented out code

--- src/backend/tree.js
+++ src/backend/tree.js
@@ -16,63 +16,6 @@ class TreeNode {
   }
 }
 
-/*ITERATIVE ATTEMPT --- DOESN'T WORK */
-// class Tree {
-
-//   constructor(rootFiberNode) {
-//     this.root = null;
-//     this.buildTree(rootFiberNode);
-//   }
-
-//   buildTree(rootFiberNode) {
-//     function traverse(fiberNode, parentTreeNode, tree) {
-//       const queue = [fiberNode];
-//       const visited = new Set();
-//       let lastParentNode = parentTreeNode; //to track new parent node for child nodes more than 1 layer below root
-
-//       while (queue.length > 0) {
-//         // Pop the first FiberNode from the queue
-//         let currFiberNode = queue.shift();
-
-//         // Create a TreeNode using the FiberNode from the queue
-//         const newNode = new TreeNode(currFiberNode.data);
-
-//         // If parentTreeNode is null, set the root on the tree
-//         if (parentTreeNode === null) {
-//           tree.root = newNode;
-//           parentTreeNode = tree.root;
-//         }
-//         // Else, add the new TreeNode to the parent's children array
-//         else {
-//           parentTreeNode.addChild(newNode);
-//         }
-
-//         // Need to add all of the siblings to the queue
-//         let curSiblingFiberNode = currFiberNode.sibling;
-//         while (curSiblingFiberNode !== null && curSiblingFiberNode !== undefined) {
-//           if (!visited.has(curSiblingFiberNode)) {
-//             queue.push(curSiblingFiberNode);
-//             visited.add(curSiblingFiberNode);
-//           }
-//           curSiblingFiberNode = curSiblingFiberNode.sibling;
-//         }
-
-//         // If fiberNode has a child, add it to the queue
-//         if (currFiberNode.child) {
-//           queue.push(currFiberNode.child);
-//           lastParentNode = parentTreeNode;
-//           parentTreeNode = newNode;
-//         }
-//       }
-//     }
-
-//     traverse(rootFiberNode, null, this);
-//   }
-
-        
-// }
-
-
 class Tree {
   constructor(rootFiberNode) {
     this.root = null;
