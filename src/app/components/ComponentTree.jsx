c50183df98dbc2a37c9bf87692ac5964ee4a8ee1
Michael Arita <michael.arita01@gmail.com>
2023-04-10 15:13:54 -0700

    feat: beginning to handle click events on renderEvent chart Co-authored-by: Jamie Lee jaamiieelee@gmail.com Co-authored-by: Nancy Yu nancyyu09@gmail.com

--- src/app/components/ComponentTree.jsx
+++ src/app/components/ComponentTree.jsx
@@ -10,168 +10,168 @@ import ReactFlow, {
   edgeStyle,
 } from 'reactflow';
 import dagre from 'dagre';
+import createTree from '../test';
+// const edges = [
+//   {
+//     id: '1-2',
+//     source: '1',
+//     target: '2',
+//     type: 'step',
+//   },
+//   {
+//     id: '2-3',
+//     source: '2',
+//     target: '3',
+//     type: 'step',
+//   },
+//   {
+//     id: '2-4',
+//     source: '2',
+//     target: '4',
+//     type: 'step',
+//   },
+//   {
+//     id: '2-5',
+//     source: '2',
+//     target: '5',
+//     type: 'step',
+//   },
+//   {
+//     id: '3-6',
+//     source: '3',
+//     target: '6',
+//     type: 'step',
+//   },
+//   {
+//     id: '3-7',
+//     source: '3',
+//     target: '7',
+//     type: 'step',
+//   },
+//   {
+//     id: '3-8',
+//     source: '3',
+//     target: '8',
+//     type: 'step',
+//   },
+//   {
+//     id: '4-9',
+//     source: '4',
+//     target: '9',
+//     type: 'step',
+//   },
+//   {
+//     id: '4-10',
+//     source: '4',
+//     target: '10',
+//     type: 'step',
+//   },
+//   {
+//     id: '4-11',
+//     source: '4',
+//     target: '11',
+//     type: 'step',
+//   },
+//   {
+//     id: '5-12',
+//     source: '5',
+//     target: '12',
+//     type: 'step',
+//   },
+//   {
+//     id: '5-13',
+//     source: '5',
+//     target: '13',
+//     type: 'step',
+//   },
+//   {
+//     id: '5-14',
+//     source: '5',
+//     target: '14',
+//     type: 'step',
+//   },
+// ];
 
-const edges = [
-  {
-    id: '1-2',
-    source: '1',
-    target: '2',
-    type: 'step',
-  },
-  {
-    id: '2-3',
-    source: '2',
-    target: '3',
-    type: 'step',
-  },
-  {
-    id: '2-4',
-    source: '2',
-    target: '4',
-    type: 'step',
-  },
-  {
-    id: '2-5',
-    source: '2',
-    target: '5',
-    type: 'step',
-  },
-  {
-    id: '3-6',
-    source: '3',
-    target: '6',
-    type: 'step',
-  },
-  {
-    id: '3-7',
-    source: '3',
-    target: '7',
-    type: 'step',
-  },
-  {
-    id: '3-8',
-    source: '3',
-    target: '8',
-    type: 'step',
-  },
-  {
-    id: '4-9',
-    source: '4',
-    target: '9',
-    type: 'step',
-  },
-  {
-    id: '4-10',
-    source: '4',
-    target: '10',
-    type: 'step',
-  },
-  {
-    id: '4-11',
-    source: '4',
-    target: '11',
-    type: 'step',
-  },
-  {
-    id: '5-12',
-    source: '5',
-    target: '12',
-    type: 'step',
-  },
-  {
-    id: '5-13',
-    source: '5',
-    target: '13',
-    type: 'step',
-  },
-  {
-    id: '5-14',
-    source: '5',
-    target: '14',
-    type: 'step',
-  },
-];
+// const nodes = [
+//   {
+//     id: '1', // required
+//     position: { x: 0, y: 0 }, // required
+//     data: { label: 'App' },
+//   },
+//   {
+//     id: '2',
+//     position: { x: 0, y: 100 },
+//     data: { label: 'Board' },
+//   },
 
-const nodes = [
-  {
-    id: '1', // required
-    position: { x: 0, y: 0 }, // required
-    data: { label: 'App' },
-  },
-  {
-    id: '2',
-    position: { x: 0, y: 100 },
-    data: { label: 'Board' },
-  },
-
-  {
-    id: '3',
-    position: { x: -600, y: 200 },
-    data: { label: 'Row' },
-  },
-  {
-    id: '4',
-    position: { x: 0, y: 200 },
-    data: { label: 'Row' },
-  },
-  {
-    id: '5',
-    position: { x: 600, y: 200 },
-    data: { label: 'Row' },
-  },
-  {
-    id: '6',
-    position: { x: -800, y: 300 },
-    data: { label: 'Box' },
-  },
-  {
-    id: '7',
-    position: { x: -600, y: 300 },
-    data: { label: 'Box' },
-  },
-  {
-    id: '8',
-    position: { x: -400, y: 300 },
-    data: { label: 'Box' },
-  },
-  {
-    id: '9',
-    position: { x: -200, y: 300 },
-    data: { label: 'Box' },
-  },
-  {
-    id: '10',
-    position: { x: 0, y: 300 },
-    data: { label: 'Box' },
-  },
-  {
-    id: '11',
-    position: { x: 200, y: 300 },
-    data: { label: 'Box' },
-  },
-  {
-    id: '12',
-    position: { x: 400, y: 300 },
-    data: { label: 'Box' },
-  },
-  {
-    id: '13',
-    position: { x: 600, y: 300 },
-    data: { label: 'Box' },
-  },
-  {
-    id: '14',
-    position: { x: 800, y: 300 },
-    data: { label: 'Box' },
-  },
-];
+//   {
+//     id: '3',
+//     position: { x: -600, y: 200 },
+//     data: { label: 'Row' },
+//   },
+//   {
+//     id: '4',
+//     position: { x: 0, y: 200 },
+//     data: { label: 'Row' },
+//   },
+//   {
+//     id: '5',
+//     position: { x: 600, y: 200 },
+//     data: { label: 'Row' },
+//   },
+//   {
+//     id: '6',
+//     position: { x: -800, y: 300 },
+//     data: { label: 'Box' },
+//   },
+//   {
+//     id: '7',
+//     position: { x: -600, y: 300 },
+//     data: { label: 'Box' },
+//   },
+//   {
+//     id: '8',
+//     position: { x: -400, y: 300 },
+//     data: { label: 'Box' },
+//   },
+//   {
+//     id: '9',
+//     position: { x: -200, y: 300 },
+//     data: { label: 'Box' },
+//   },
+//   {
+//     id: '10',
+//     position: { x: 0, y: 300 },
+//     data: { label: 'Box' },
+//   },
+//   {
+//     id: '11',
+//     position: { x: 200, y: 300 },
+//     data: { label: 'Box' },
+//   },
+//   {
+//     id: '12',
+//     position: { x: 400, y: 300 },
+//     data: { label: 'Box' },
+//   },
+//   {
+//     id: '13',
+//     position: { x: 600, y: 300 },
+//     data: { label: 'Box' },
+//   },
+//   {
+//     id: '14',
+//     position: { x: 800, y: 300 },
+//     data: { label: 'Box' },
+//   },
+// ];
 
 // also need totalRenderDurationMS
 // renderDurationMS
 
 const ComponentTree = () => {
-  // const nodes = [];
-  // const edges = [];
+  const nodes = [];
+  const edges = [];
 
   // // Breadth first search
   // // Create a node for the current tree node we're looking at
--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -1,18 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
+import dagre from 'dagre';
 
 import ComponentTree from './ComponentTree';
 import RenderEvents from './RenderEvents';
 import ComponentsRanked from './ComponentsRanked';
 import RenderedComponents from './RenderedComponents';
 
+
 function Dashboard(props) {
+  // State variables
+  // This will hold nodes and edges for each renderEvent tree in the form of an object
+  //  with properties nodes and edges.
+  const [nodesAndEdges, setNodesAndEdges] = useState([]);
+  // Hold the current nodesAndEdge object that is to be displayed in the componentTree
+  const [flowDisplayTree, setFlowDisplayTree] = useState({});
+
+  // // Breadth first search
+  // // Create a node for the current tree node we're looking at
+  // // Create the edge based on .parent and .data
+  const createNodesAndEdges = (root) => {
+    const nodes = [];
+    const edges = [];
+
+    // Traverse through the tree using breadth first search
+    const bfsQueue = [root];
+    const idQueue = [];
+  
+    // Dagre graph setup code
+    const width = 144;
+    const height = 100;
+    let dagreGraph = new dagre.graphlib.Graph({ rankdir: 'TB' });
+    dagreGraph.setGraph({});
+    dagreGraph.setDefaultEdgeLabel(function () {
+      return {};
+    });
+  
+    let id = 1;
+    // Using breadth first search to look through the tree
+    while (bfsQueue.length > 0) {
+      const treeNode = bfsQueue.shift();
+      // Create a node for the current Tree node
+      dagreGraph.setNode(id, { label: treeNode.componentName, width, height });
+  
+      if (idQueue.length > 0) {
+        const parentId = idQueue.shift();
+        dagreGraph.setEdge(parentId, id);
+      }
+  
+      if (treeNode.children.length > 0) {
+        bfsQueue.push(...treeNode.children);
+  
+        // Push ID into isQueue for the amount of children there are
+        for (let i = 0; i < treeNode.children.length; i++) {
+          idQueue.push(id);
+        }
+      }
+      id++;
+    }
+    dagre.layout(dagreGraph);
+  
+    dagreGraph.nodes().forEach((nodeId) => {
+      const node = dagreGraph.node(nodeId);
+      nodes.push({
+        id: nodeId.toString(),
+        data: { label: node.label },
+        position: { x: node.x, y: node.y },
+      });
+    });
+    dagreGraph.edges().forEach((edgeObj) => {
+      const { v: sourceNodeId, w: targetNodeId } = edgeObj;
+      edges.push({
+        id: `${sourceNodeId}-${targetNodeId}`,
+        source: sourceNodeId.toString(),
+        target: targetNodeId.toString(),
+      });
+    });
+    // Package node and edges into object then store into state within an array
+    return {
+      nodes,
+      edges,
+    };
+  };
+
+  const { renderEventList } = props.reaperSession;
+  // For the amount of renderEvents
+  for (let i = 0; i < renderEventList.length; i++) {
+    nodesAndEdges.push(createNodesAndEdges(renderEventList.tree));
+    setNodesAndEdges(nodesAndEdges);
+  }
+
   // console.log('Dashboard.jsx, reaperSessionObj: ', props.reaperSessionObj);
   return (
     <div id='content'>
       <div className='row'>
         <div className='column'>
           <div className='graph'>
-            <RenderEvents />
+            <RenderEvents nodesAndEdges={nodesAndEdges} setFlowDisplayTree={setFlowDisplayTree} />
           </div>
         </div>
         <div className='column'>
@@ -29,7 +112,7 @@ function Dashboard(props) {
         </div>
         <div className='column'>
           <div className='graph'>
-            <ComponentTree />
+            <ComponentTree displayTree={flowDisplayTree} />
           </div>
         </div>
       </div>
