eb9e7991d5b2a52b7c7d4009ebeef9daa6a27423
Michael Arita <michael.arita01@gmail.com>
2023-04-11 16:26:38 -0700

    chore: merging changes from dev Co-authored-by: Jamie Lee jaamiieelee@gmail.com

--- src/app/components/RenderEvents.jsx
+++ src/app/components/RenderEvents.jsx
@@ -1,26 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Chart as ChartJS } from 'chart.js/auto';
 import { Bar } from 'react-chartjs-2';
+import { render } from 'react-dom';
 
 function RenderEvents(props) {
   const [clickedBarData, setClickedBarData] = useState(null);
-  const backgroundColor = [
-    'rgba(255, 99, 132, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(255, 205, 86, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(54, 162, 235, 0.2)',
-  ];
-  const borderColor = [
-    'rgb(255, 99, 132)',
-    'rgb(255, 159, 64)',
-    'rgb(255, 205, 86)',
-    'rgb(75, 192, 192)',
-    'rgb(54, 162, 235)',
-  ];
-
-  const data = {
-    labels: ['12:1719.41', '+00:02.19', '+00:01.41', '+00:01.54', '+08:20.14', 'render event 1', 'render event 2', 'render event 3'],
+  const [chartData, setChartData] = useState({
+    labels: [],
     datasets: [
       {
         label: 'Render Events',
@@ -59,18 +45,90 @@ function RenderEvents(props) {
           'rgb(92, 184, 92)',
         ],
         borderWidth: 1,
-        data: [0.3, 0.1, 0.55, 0.6, 0.7, 0.4, 0.2, 0.3, 0.213],
+        data: [],
       },
     ],
-  };
+  });
 
-  // useEffect(() => {
-  //   console.log('RenderEvents: These are our renderTimes,', props.renderTimes);
-  // }, [props]);
+  const backgroundColor = [
+    'rgba(255, 99, 132, 0.2)',
+    'rgba(255, 159, 64, 0.2)',
+    'rgba(255, 205, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+  ];
+  const borderColor = [
+    'rgb(255, 99, 132)',
+    'rgb(255, 159, 64)',
+    'rgb(255, 205, 86)',
+    'rgb(75, 192, 192)',
+    'rgb(54, 162, 235)',
+  ];
 
-  // const createDataForChart = () => {
-    
+  // const data = {
+  //   labels: ['12:1719.41', '+00:02.19', '+00:01.41', '+00:01.54', '+08:20.14', 'render event 1', 'render event 2', 'render event 3'],
+  //   datasets: [
+  //     {
+  //       label: 'Render Events',
+  //       backgroundColor: [
+  //         // 'rgba(255, 99, 132, 0.2)',
+  //         // 'rgba(255, 159, 64, 0.2)',
+  //         // 'rgba(255, 205, 86, 0.2)',
+  //         // 'rgba(75, 192, 192, 0.2)',
+  //         // 'rgba(54, 162, 235, 0.2)',
+  //         'rgba(255, 99, 132, 0.2)',
+  //         'rgba(54, 162, 235, 0.2)',
+  //         'rgba(255, 159, 64, 0.2)',
+  //         'rgba(75, 192, 192, 0.2)',
+  //         'rgba(255, 205, 86, 0.2)',
+  //         'rgba(153, 102, 255, 0.2)',
+  //         'rgba(255, 77, 166, 0.2)',
+  //         'rgba(51, 204, 51, 0.2)',
+  //         'rgba(255, 128, 0, 0.2)',
+  //         'rgba(92, 184, 92, 0.2)',
+  //       ],
+  //       borderColor: [
+  //         // 'rgb(255, 99, 132)',
+  //         // 'rgb(255, 159, 64)',
+  //         // 'rgb(255, 205, 86)',
+  //         // 'rgb(75, 192, 192)',
+  //         // 'rgb(54, 162, 235)',
+  //         'rgb(255, 99, 132)',
+  //         'rgb(54, 162, 235)',
+  //         'rgb(255, 159, 64)',
+  //         'rgb(75, 192, 192)',
+  //         'rgb(255, 205, 86)',
+  //         'rgb(153, 102, 255)',
+  //         'rgb(255, 77, 166)',
+  //         'rgb(51, 204, 51)',
+  //         'rgb(255, 128, 0)',
+  //         'rgb(92, 184, 92)',
+  //       ],
+  //       borderWidth: 1,
+  //       data: [0.3, 0.1, 0.55, 0.6, 0.7, 0.4, 0.2, 0.3, 0.213],
+  //     },
+  //   ],
   // };
+
+  useEffect(() => {
+    console.log('RenderEvents: These are our renderTimes,', props.renderTimes);
+    const { renderTimes } = props;
+    setChartData(createDataForChart(renderTimes));
+  }, [props]);
+
+  const createDataForChart = (renderTimes) => {
+    // Create the labels for the length of renderTimes array
+    const labels = [];
+    const data = {};
+    for (let i = 0; i < renderTimes.length; i++) {
+      labels.push(`Render Event ${i + 1}`);
+    }
+
+    data.labels = labels;
+    data.datasets[0].data = renderTimes;
+
+    return data;
+  };
 
 
   // Hard code colors and cycle through it
@@ -108,7 +166,8 @@ function RenderEvents(props) {
   return (
     <div id='render-events'>
       <p>Render Events and Duration</p>
-      <Bar data={data} options={options} />
+      <Bar data={chartData} options={options} />
+      {/* <Bar /> */}
     </div>
   );
 }
