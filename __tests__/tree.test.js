8621680b9f0ed389a23fe38271197e0931a9616a
Katrina McDonagh <katrina@Katrinas-MacBook-Pro.local>
2023-04-11 15:24:50 -0700

    tree testing done

--- __tests__/tree.test.js
+++ __tests__/tree.test.js
@@ -11,3 +11,23 @@ test('Check traversal function in tree.js is correctly traversing fiberObject', 
   const tree = makeTreeFunc(exampleFiberObj);
   expect(tree).toEqual(exampleTree);
 })
+
+test('Does not break if a property is not present on fiber Obj', () => {
+  const incompleteFiberObj = {
+    sibling: null,
+    elementType: null,
+    actualDuration: 2.100000023841858,
+    actualStartTime: 3505.4000000059605,
+    memoizedProps: null,
+    tag: 3, 
+    selfBaseDuration: 1.300000011920929
+  };
+  const tree = makeTreeFunc(incompleteFiberObj);
+  expect(tree).toBeDefined;
+})
+
+test('output tree has a root property', () => {
+  const tree = makeTreeFunc(exampleFiberObj);
+  expect(tree).toHaveProperty('root');
+
+})
