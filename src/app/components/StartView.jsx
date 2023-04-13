917cfe34162d1c354b53a9662a9fb18f38f00d5c
Michael Arita <michael.arita01@gmail.com>
2023-04-13 11:40:54 -0700

    feat: added in reaper logo and new file-loader for webpack

--- src/app/components/StartView.jsx
+++ src/app/components/StartView.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSkull } from '@fortawesome/free-solid-svg-icons';
+import reaperLogo from './reaper-128.png';
 
 function StartView() {
   return (
     <div className='start-view'>
       <div className='skull-icon-container'>
-        <FontAwesomeIcon icon={faSkull} size='4x' color='white' />
+        {/* <FontAwesomeIcon icon={faSkull} size='4x' color='white' /> */}
+        <img src={reaperLogo} alt="reaper logo" />
       </div>
       <div className='start-text'>
         No Session data to display. Click on the record button to start a
